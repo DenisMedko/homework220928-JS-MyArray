af7cd3fe4b73e66ecb8e12550740a049e28a8a47
DenisMedko <medko.denis76@gmail.com>
2022-10-02 13:33:37 +0300

    unshift and shift methods added

--- assets/js/index.js
+++ assets/js/index.js
@@ -70,13 +70,59 @@ class MyArray {
   static isMyArray(obj) {
     return obj instanceof MyArray;
   }
+
+  cleanObject() {
+    for (let i = 0; i < this.length; i++) {
+      delete this[i];  
+    }
+    this.length = 0;
+  }
+
+  unshift(...elems) {
+    // добавить элементы в начало объекта
+    // создаем временный объект
+    const tmpMyArray = new MyArray();
+    tmpMyArray.push(...this);
+    // очищаем объект
+    this.cleanObject();
+    // добавляем в начало элементы
+    this.push(...elems);
+    // дополняем исходными элементами
+    this.push(...tmpMyArray);
+    //  возвращает новую длину объекта
+    return this.length;
+  }
+
+  shift() {
+    if (this.length === 0) {
+      return undefined;
+    }
+    // создаем временный объект
+    const tmpMyArray = new MyArray();
+    tmpMyArray.push(...this);
+    // очищаем объект
+    this.cleanObject();
+    // запоминаем удаляемый элемент
+    const deletedValue = tmpMyArray[0];
+    // удаляем 1-й элемент
+    delete tmpMyArray[0];
+    // заполняем объект из копии со 2-го элемента
+    for (let i = 1; i < tmpMyArray.length; i++) {
+      this.push(tmpMyArray[i]);
+    }
+    return deletedValue;
+  }
 }
+
 
 const myArr = new MyArray();
 myArr.push(10, 20, 30);
 const myArr1 = new MyArray();
 myArr1.push(10000, 20000, [1, 2, [3, 4, 5]]);
 const myArr2 = myArr.concat(null, 40, 50, 60, myArr1).concat(6, [7, 8, [9, 10, 11]]);
+
+myArr.unshift(40, 50, 60);
+myArr.shift();
 
 //const myArr2 = myArr.concat(6, [7, 8, [9, 10, 11]]);
 
